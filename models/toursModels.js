376f0d5225d45df33a3b079755e9f1ee55cb589e
chrisfelicien <f.ilungakalenga@gmail.com>
2023-06-14 12:51:13 +0200

    Changing the database

--- models/toursModels.js
+++ models/toursModels.js
@@ -1,21 +1,38 @@
 import mongoose from "mongoose";
 
 const tourSchema = new mongoose.Schema({
-  title: {
+  name: {
     type: String,
-    maxlength: [50, "Tour name must contains not more than 50 characters"],
+    maxlength: [20, "Tour name must contains not more than 50 characters"],
     minlength: [5, "Tour name must have at least 5 characters"],
     required: [true, "Tour must have a title"],
-    unique: [true, "This name is already used"],
   },
-  startDates: {
-    type: [Date],
-    required: [true, "Tour must have a starting date"],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
   },
   price: {
     type: Number,
     required: [true, "Tour must have a price"],
   },
+  company: {
+    type: String,
+    enum: {
+      values: ["ikea", "liddy", "marcos", "caressa"],
+      message: "{VALUE} is unknown company",
+    },
+    required: [true, "Please provide a company name"],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+    min: [4, "The min of rating is 4"],
+    // max: [5.0, "Max rating is 4.0"],
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-export default mongoose.model("TourModel", tourSchema);
+export default mongoose.model("Products", tourSchema);
